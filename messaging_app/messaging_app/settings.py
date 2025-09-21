fdbd08bc36388bfca4ac0887fdec5cab9f8ee8d8
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-21 01:24:25 +0000

    Add Django models for User, Conversation, and Message with UUID primary keys

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,12 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.permissions.IsAuthenticated',
-    'DEFAULT_AUTHENTICATION_CLASSES',
-    'rest_framework.authentication.SessionAuthentication',
-
-
+    'chats',
 ]
+
+# Custom user model
+AUTH_USER_MODEL = 'chats.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
