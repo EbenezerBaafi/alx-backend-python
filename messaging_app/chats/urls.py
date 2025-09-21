3b063667e7c74beb92f4303f96e2e0bc039b36ca
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-21 02:16:25 +0000

    App running smoothly

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,15 +1,14 @@
-# chats/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
 
 #routers.DefaultRouter()
