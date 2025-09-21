4dfb5162b42e8f13b45407cee3b3253c9b0e2dbf
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-21 01:06:11 +0000

    Add rest_framework to the installed_apps in settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.permissions.IsAuthenticated',
+    'DEFAULT_AUTHENTICATION_CLASSES',
+    'rest_framework.authentication.SessionAuthentication',
+
+
 ]
 
 MIDDLEWARE = [
