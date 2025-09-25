fc861e29dfdcda9e3e3423a26d78f7c1615bd0b3
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-25 22:38:54 +0000

    Added the RolePermissionMiddleware to the MIDDLEWARE in the settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -31,8 +31,7 @@ MIDDLEWARE = [
     'chats.middleware.RequestLoggingMiddleware', 
     'chats.middleware.RestrictAccessByTimeMiddleware',
     'chats.middleware.OffensiveLanguageMiddleware', 
- 
-
+    'chats.middleware.RolepermissionMiddleware'
 
 ]
 
