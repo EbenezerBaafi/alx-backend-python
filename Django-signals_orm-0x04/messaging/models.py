f5739a835332426a1a36190bf0e8870d17254bdc
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-30 00:28:53 +0000

    Added model fields to the messaging app

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import Users
 
 # Create your models here.
+
+# Model to store messages between users
+class Message(models.Model):
+    sender = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='sent_messages')
+    reciever = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.recipient} at {self.timestamp}"
+    
+    class Meta:
+        ordering = ['-timestamp']
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"
+
+# Model to store notifications for users
+class Notification(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='notifications')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='notifications')
+    Notification_text = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user} - Read: {self.read}"
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Notification"
+        verbose_name_plural = "Notifications"
+
+    def mark_as_read(self):
+        self.is_read = True
+        self.save()
